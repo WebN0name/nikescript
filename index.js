e28dcfcbbef3fedff07181c721c85adfdd7396b5
WebN0name <Haswelvc@ya.ru>
2020-02-04 20:48:58 +0700

    v2

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
-const {Builder, By,Key} = require("selenium-webdriver");
+const puppeteer = require('puppeteer');
 
-async function example(email, password){
-    let driver = new Builder().forBrowser("firefox").build();
-    await driver.get("https://www.nike.com/ru/");
-    await (await driver.findElement(By.className("nav-btn"))).click();
-    await driver.findElement(By.name("emailAddress")).sendKeys(email);
-    await driver.findElement(By.name("password")).sendKeys(password);
-    setTimeout(() => {
-        driver.findElement(By.xpath("//input[@value='ВОЙТИ']")).click();
-    }, 5000)
+function delay(time) {
+    return new Promise(function(resolve) { 
+        setTimeout(resolve, time)
+    });
+ }
+
+async function main(login, password, num){
+  const browser = await puppeteer.launch(
+    {headless: false, 
+      'defaultViewport' : { 'width' : 1024, 'height' : 1600 }
+  });
+  const page = await browser.newPage();
+  await page.goto('https://www.nike.com/ru/', {
+    waitUntil: 'networkidle2',
+  });
+  await page.click("button[data-type=click_navJoinLogin]");
+  await page.type('input[name=emailAddress]', login, {delay: 20});
+  await page.type('input[name=password]', password, {delay: 20});
+  await delay(4000);
+  await page.click("input[value=ВОЙТИ]");
+  await delay(4000);
+  await page.goto('https://www.nike.com/ru/launch/t/dunk-low-plum/', {
+    waitUntil: 'networkidle2',
+  });
+  await page.screenshot({path: 'browser' + num + '.png'});
+  await browser.close();
+};
+
+let login = ['neytronoman@gmail.com', 'iskandarakhm@gmail.com', 'Haswelvc@yandex.ru', /*'takhmdezhanovrs@gmail.com', 'resalle@bk.ru', 'david.kundeutschrus@gmail.com'*/];
+    let password = ['Electron99', 'BelarA123456', 'Electron99', /*'BelarA123', 'BElarA123', 'Kun48434348'*/];   
+
+let i = 0;                   
+
+function myLoop () {           
+   setTimeout(function () {          
+    main(login[i], password[i], i+1)  
+    i++;                    
+      if (i < login.length) {            
+         myLoop();             
+      }                        
+   }, 2000)
 }
 
-let emails = ['neytronoman@gmail.com', 'iskandarakhm@gmail.com', 'Haswelvc@yandex.ru'];
-let passwords = ['Electron99', 'BelarA123456', 'Electron99'];
+myLoop(); 
 
 
-for(let i = 0; i< emails.length; i++){
-    example(emails[i], passwords[i]);
-}
 
 
 
